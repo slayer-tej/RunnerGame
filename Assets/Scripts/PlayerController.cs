23337566178f15aaeb6587b6be18a51c5a7d787e
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-22 16:18:58 +0530

    Basic Rig Tweaks for models of Character,Environment,Character animations and setup player run along z axis

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rbd;
+    [SerializeField]
+    private int speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
