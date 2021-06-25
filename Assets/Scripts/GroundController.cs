07a97b64afb5aba1a33456f7cd3d82acb89b1553
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-25 16:44:40 +0530

    added infinte Ground using Object Pooling and generated random obstacles for each time the object is returned from pool

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+    public Transform spawnPosition;
+    [SerializeField]
+    public Transform startPoint;
+    [SerializeField]
+    public Transform leftPoint;
+    [SerializeField]
+    public Transform rightPoint;
+    [SerializeField]
+    public Transform endPoint;
+    [SerializeField]
+    private GameObject[] obstacles;
+
+    private bool isFromPool = false;
+    private List<GameObject> pooledItems = new List<GameObject>();
+    private int numOfObstaclesToPlace;
+
+    private void Awake()
+    {
+        numOfObstaclesToPlace = Random.Range(5,10);
+    }
+
+    public void ActivateRandomObstacle()
+    {
+        for(int i = 0; i < numOfObstaclesToPlace; i++)
+        {
+            int randomNumber = Random.Range(0, obstacles.Length);
+            Vector3 randomPos = new Vector3(Random.Range(leftPoint.position.x, rightPoint.position.x), 0, Random.Range(startPoint.position.z, endPoint.position.z));
+            if(!isFromPool)
+            {
+                GameObject obstacle = Instantiate(obstacles[randomNumber], randomPos, Quaternion.identity, spawnPosition);
+                pooledItems.Add(obstacle);
+                obstacle.SetActive(true);
+            }
+            else
+            {
+                pooledItems[i].transform.position = randomPos;
+                pooledItems[i].SetActive(true);
+            }
+        }
+        isFromPool = true;
+    }
+
+    public void DeactivateAllObstacles()
+    {
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            obstacles[i].SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            other.gameObject.GetComponent<PlayerController>().CreateGround();
+        }
+    }
+
+    public void Enable()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,126 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
+    private CharacterController controller;
     private Rigidbody rbd;
+    private Vector3 startPos;
+    private Animator _anim;
+    private bool fingerDown;
+    private int currentLane = 1;
     [SerializeField]
-    private int speed = 20;
+    private float laneDistance = 4f;
+    private float verticalVelocity = -0.1f;
+
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameController gameController;
+    [SerializeField]
+    private float horizontalSpeed;
+    [SerializeField]
+    private int pixelDistToDetect;
+
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float gravity;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rbd = gameObject.GetComponent<Rigidbody>();
+        controller = gameObject.GetComponent<CharacterController>();
+        _anim = gameObject.GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        //Vector3 verticalMovement = Vector3.forward * speed * Time.fixedDeltaTime;
+        //rbd.MovePosition(transform.position +  verticalMovement);
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        SwipeControls();
+        TotalDistanceCovered();
+    }
+
+    private void SwipeControls()
+    {
+        if (!fingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            startPos = Input.touches[0].position;
+            fingerDown = true;
+        }
+
+        if (fingerDown && Input.touches[0].phase == TouchPhase.Moved)
+        {
+            if (Input.touches[0].position.y >= startPos.y + pixelDistToDetect)
+            {
+                verticalVelocity = -0.1f;
+                _anim.SetTrigger("isJumped");
+                verticalVelocity = jumpForce;
+                fingerDown = false;
+            }
+            else if (Input.touches[0].position.x <= startPos.x - pixelDistToDetect)
+            {
+                MoveLane(false);
+                _anim.SetTrigger("isMovedLeft");
+                fingerDown = false;
+                Debug.Log("swipeleft");
+            }
+            else if (Input.touches[0].position.x >= startPos.x + pixelDistToDetect)
+            {
+                MoveLane(true);
+                _anim.SetTrigger("isMovedRight");
+                fingerDown = false;
+                Debug.Log("swipeRight");
+            }
+            else if (Input.touches[0].position.y <= startPos.y - pixelDistToDetect)
+            {
+                _anim.SetTrigger("isSliding");
+                fingerDown = false;
+            }
+        }
+
+        if (fingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended)
+        {
+            fingerDown = false;
+        }
+
+        Vector3 targetPos = transform.position.z * Vector3.forward;
+        if (currentLane == 0)
+            targetPos += Vector3.left * laneDistance;
+        else if (currentLane == 2)
+            targetPos += Vector3.right * laneDistance;
+
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPos - transform.position).normalized.x * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+        currentLane += (goingRight) ? 1 : -1;
+        currentLane = Mathf.Clamp(currentLane, 0, 2);
+    }
+
+    public void CreateGround()
+    {
+        gameController.GetItemFromPool();
+    }
+    public void TotalDistanceCovered()
+    {
+        gameController.playerDistance =  this.transform.position.z;
     }
 }
